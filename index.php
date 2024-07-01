6ee2b2aa5d666de81446ce1d4bbd307ea446c1fa
CristianN01 <cristiannechifor71@gmail.com>
2024-07-01 16:18:06 +0200

    aggiunto vue via cdn, e creato cartella js

--- index.php
+++ index.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP ToDo List JSON </title>
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 <body>
-    
+    <header>
+        <h1>
+            PHP ToDo List JSON
+        </h1>
+    </header>    
+
+    <main>
+
+    </main>
+
 </body>
 </html>
