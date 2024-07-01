219e5af017b8f4f810edd62dd2e0401c743133e5
CristianN01 <cristiannechifor71@gmail.com>
2024-07-01 16:20:47 +0200

    aggiunto bootstrap stramite cdn

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP ToDo List JSON </title>
-
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <header>
@@ -13,10 +13,11 @@
             PHP ToDo List JSON
         </h1>
     </header>    
-
-    <main>
-
+    
+    <main id='app'>
+        
     </main>
-
+        
+        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </body>
 </html>
